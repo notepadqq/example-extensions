cdc8216e7755d712b04efdbe0dae88b24749664a
Daniele Di Sarli <danieleds0@gmail.com>
2015-04-30 17:56:13 +0200

    Updated to use notepadqqApi.onWindowCreated

--- guid-generator/main.rb
+++ guid-generator/main.rb
@@ -7,36 +7,9 @@ require 'securerandom'
 
 # Start the event loop
 @notepadqqApi.runEventLoop do
-  
-  # Get a reference to the main Notepadqq object
-  @nqq = @notepadqqApi.notepadqq
-  
-  # Array of already initialized windows
-  @initializedWindows = []
 
-  # As soon as the extension is started, initialize
-  # all the opened windows.
-  @nqq.on(:currentExtensionStarted) do
-    @nqq.windows.each do |window|
-      initWindow(window)
-    end
-  end
-  
   # Each time a new window gets opened, initialize it.
-  # When Notepadqq is starting and initializing all the extensions,
-  # we might not be fast enough to receive this event: this is why
-  # we initialize windows on currentExtensionStarted too.
-  @nqq.on(:newWindow) do |window|
-    initWindow(window)
-  end
-  
-  def initWindow(window)
-    # It could happen that we try to initialize this window twice:
-    # on @nqq.newWindow and on @nqq.currentExtensionStarted.
-    # We avoid it by keeping track of which window we already initialized.
-    return if @initializedWindows.include? window
-    @initializedWindows.push window
-    
+  @notepadqqApi.onWindowCreated do |window|
     # Add a menu item
     menu = window.addExtensionMenuItem(@notepadqqApi.extensionId, "Generate GUID")
     menu.on(:triggered) do
