9837f2d69719fe75aa1e0e22d083803652b9bc75
Daniele Di Sarli <danieleds0@gmail.com>
2015-06-09 11:52:54 +0200

    Update

--- guid-generator/main.js
+++ guid-generator/main.js
@@ -1,18 +1,20 @@
-var NotepadqqApi = require("notepadqq-api")
+var NotepadqqApi = require("notepadqq-api").NotepadqqApi
 var uuid = require('uuid');
 
 // Connect to notepadqq
-var api = new NotepadqqApi.NotepadqqApi();
+NotepadqqApi.connect(function(api) {
 
-// We initialize each window here
-api.onWindowInitialization(function(window) {
-	
-	// Add a new menu item
-	var menu = window.addExtensionMenuItem(api.extensionId, "Generate GUID")
+    // We initialize each window here
+    api.onWindowInitialization(function(window) {
 
-	// Add an handler to the "triggered" event of the menu item
-	menu.on("triggered", function() {
-		window.currentEditor().setSelectionsText([uuid.v4()]);
-	});
-	
+        // Add a new menu item
+        var menu = window.addExtensionMenuItem(api.extensionId, "Generate GUID")
+
+        // Add an handler to the "triggered" event of the menu item
+        menu.on("triggered", function() {
+            window.currentEditor().setSelectionsText([uuid.v4()]);
+        });
+
+    });
+
 });
